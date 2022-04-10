8a793f440af8405b0b6371a4afcd53aa9c4bf930
maks <maks.rus.in@mail.ru>
2022-04-10 15:08:22 +0300

    Lesson2 is done

---
+++ todolist/userapp/management/commands/create_super_user.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from ...models import UserProfile, User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        users = User.objects.all()
+        for user in users:
+            if not len(UserProfile.objects.filter(user=user)):
+                users_profile = UserProfile.objects.create(user=user)
+                users_profile.save()
